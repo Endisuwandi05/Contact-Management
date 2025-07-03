3172e490e53d17ee73ec63ec5767f7dc5066fec2
Endi Suwandi <endisuwandi64@gmail.com>
2025-07-03 20:30:12 +0700

    improve main js

--- main.js
+++ main.js
@@ -2,56 +2,56 @@
 // Kontak terdiri dari nama, email, dan nomor telepon
 let contacts = [
   {
-    id: 1,
+    id: Date.now(),
     name: "Endi Suwandi",
     email: "endisuwandi@gmail.com",
     phone: "+6281234567890",
     address: "Jl. Contoh Alamat No. 1, Kota, Indonesia",
   },
   {
-    id: 2,
+    id: Date.now(),
     name: "Rizky Pratama",
     email: "rizky.pratama@example.com",
     phone: "+6281122334455",
     address: "Jl. Kenanga No. 10, Jakarta, Indonesia",
   },
   {
-    id: 3,
+    id: Date.now(),
     name: "Siti Nurhaliza",
     email: "siti.nurhaliza@example.com",
     phone: "+6285566778899",
     address: "Jl. Mawar No. 5, Surabaya, Indonesia",
   },
   {
-    id: 4,
+    id: Date.now(),
     name: "Ahmad Fauzi",
     email: "ahmad.fauzi@example.com",
     phone: "+6282233445566",
     address: "Jl. Anggrek No. 12, Yogyakarta, Indonesia",
   },
   {
-    id: 5,
+    id: Date.now(),
     name: "Lestari Putri",
     email: "lestari.putri@example.com",
     phone: "+6287788990011",
     address: "Jl. Dahlia No. 8, Semarang, Indonesia",
   },
   {
-    id: 6,
+    id: Date.now(),
     name: "Bagas Saputra",
     email: "bagas.saputra@example.com",
     phone: "+6283344556677",
     address: "Jl. Flamboyan No. 3, Medan, Indonesia",
   },
   {
-    id: 7,
-    name: "Putri Ayu",
+    id: Date.now(),
+    name: "Siti Badriah",
     email: "putri.ayu@example.com",
     phone: "+6289900112233",
     address: "Jl. Bougenville No. 15, Bali, Indonesia",
   },
   {
-    id: 8,
+    id: Date.now(),
     name: "Dimas Prabowo",
     email: "dimas.prabowo@example.com",
     phone: "+6281234567890",
@@ -62,7 +62,7 @@ let contacts = [
 // Fitur menambahkan kontak
 function addContact(name, email, phone, address) {
   let contact = {
-    id: contacts.length + 1,
+    id: Date.now(),
     name: name,
     email: email,
     phone: phone,
@@ -80,7 +80,7 @@ function showAllContacts() {
     contacts.forEach((contact) => {
       console.log(
         `
-ID: ${contact.id}:
+// ID: ${contact.id}:
 Name: ${contact.name}
 Email: ${contact.email}
 Phone: ${contact.phone}
@@ -112,4 +112,12 @@ Address: ${contact.address}`
   }
 }
 
+//Fitur delete contact
+function DeleteContact(id) {
+  contacts = contacts.filter((contact) => contact.id !== id);
+  console.log("Contact deleted successfully.");
+}
+
 showAllContacts();
+addContact();
+DeleteContact();
